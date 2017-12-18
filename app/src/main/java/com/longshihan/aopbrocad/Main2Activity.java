ba5a19492973e12f797d85bf10059b704a51d2d5
longhe <LONGHE001@huazhu.com>
2017-12-18 14:08:36 +0800

    上传jcenter

--- app/src/main/java/com/longshihan/aopbrocad/Main2Activity.java
+++ app/src/main/java/com/longshihan/aopbrocad/Main2Activity.java
@@ -1,11 +1,9 @@
 package com.longshihan.aopbrocad;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.TextView;
 
